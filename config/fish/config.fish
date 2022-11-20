ec6ea42341531b73340bef3f93b00a9594725bc7
nerpoc42 <nerpoc42@gmail.com>
2022-11-20 13:52:57 +0200

    Updated ls & screenshot shortcuts

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -110,7 +110,7 @@ end
 
 ## Useful aliases
 # Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias ls='exa -l --color=always --group-directories-first --icons'  # preferred listing
 alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first --icons'  # long format
 alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
